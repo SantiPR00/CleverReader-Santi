05ab74939def73f908c01e287a0a6c4d59359c36
SantiPR00 <santiperez3@gmail.com>
2022-11-30 11:32:03 +0100

    Clippy-46

--- api/views.py
+++ api/views.py
@@ -1,6 +1,15 @@
 from django.http import HttpResponse
+from api import reference
 
 def consume_file(request):
     if request.method == 'POST':
         print(request.FILES['file'])
-        return HttpResponse(request.FILES['file'].name)
+        file = request.FILES['file']
+        extension = request.FILES['file'].name.split(".")
+        extension[1] != "pdf"
+        extension = ["sample", "pdf"]
+        if(extension[1] != "pdf"):
+            return HttpResponse.status_code == 400
+        else:
+            return reference.get_references(file)
+    
